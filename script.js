8005a8857f192a529369d11a3683359df786c301
KendiiiiiiiiiiX <kendixtv@gmail.com>
2024-11-29 19:23:43 +0100

    4

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 function toggleMenu() {
-  const sidebar = document.getElementById('sidebar');
-  const menuIcon = document.querySelector('.menu-icon');
-  const closeIcon = document.querySelector('.close-icon');
 
-  sidebar.classList.toggle('active');
-  
-  // Alterner entre l'icône hamburger et l'icône de fermeture
-  if (sidebar.classList.contains('active')) {
-    menuIcon.style.display = 'none';
-    closeIcon.style.display = 'block';
-  } else {
-    menuIcon.style.display = 'block';
-    closeIcon.style.display = 'none';
-  }
+ const sidebar = document
+  .getElementById('sidebar');
+
+ const menuIcon = document
+  .querySelector('.menu-icon');
+
+ const closeIcon = document
+  .querySelector('.close-icon');
+
+
+
+ sidebar.classList.toggle(
+  'active');
+
+
+
+ // Alterner entre l'icône hamburger et l'icône de fermeture
+
+ if (sidebar.classList.contains(
+   'active')) {
+
+  menuIcon.style.display = 'none';
+
+  closeIcon.style.display =
+   'block';
+
+ } else {
+
+  menuIcon.style.display =
+   'block';
+
+  closeIcon.style.display =
+   'none';
+
+ }
+
 }
